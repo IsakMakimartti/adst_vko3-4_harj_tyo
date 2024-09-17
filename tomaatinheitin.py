6b6f1b29fbabae12f2c82e0776a2037db290cc2b
IsakMakimartti <t2mais00@students.oamk.fi>
2024-09-17 19:00:46 +0300

    Nappi lisätty ja ernestin kuva tulee näkyviin

--- tomaatinheitin.py
+++ tomaatinheitin.py
@@ -1,22 +1,83 @@
 import tkinter as tk
-from tkinter import PhotoImage
+import random
+from tkinter import *
 
 tomaatinheitinIkkuna = tk.Tk()
 
 tomaatinheitinIkkuna.title("Tomaatinheitin")
-tomaatinheitinIkkuna.geometry('1200x900')
+tomaatinheitinIkkuna.geometry('1920x1080')
 
-#lbl = tk.Label(tomaatinheitinIkkuna, text="Tomaatinheitin")
-#lbl.grid()
+# Configuration for grid layout
+tomaatinheitinIkkuna.grid_rowconfigure(0, weight=0)
+tomaatinheitinIkkuna.grid_columnconfigure(0, weight=1)
 
-# Kernesti
+tomaatinheitinIkkuna.grid_rowconfigure(1, weight=1)
+tomaatinheitinIkkuna.grid_columnconfigure(1, weight=1)
+
+tomaatinheitinIkkuna.grid_rowconfigure(2, weight=0)
+tomaatinheitinIkkuna.grid_columnconfigure(2, weight=1)
+
+# Kernestis Image
 imageKernesti = PhotoImage(file="kerne.png")
 imageLabelKernesti = tk.Label(tomaatinheitinIkkuna, image=imageKernesti)
-imageLabelKernesti.grid(row=0, column=0)
+imageLabelKernesti.grid(row=random.randint(0, 2), column=0, sticky="w")
 
-# Maalitaulu
+# Maalitaulu Image
 imageMaalitaulu = PhotoImage(file="maalitaulu.png")
 imageLabelMaalitaulu = tk.Label(tomaatinheitinIkkuna, image=imageMaalitaulu)
-imageLabelMaalitaulu.grid(row=0, column=1)
+imageLabelMaalitaulu.grid(row=1, column=1)
+
+onkoPainettu = False
+
+# Ernestis Image
+def image_ernesti():
+    imageErnesti = PhotoImage(file="erne.png")
+    imageLabelErnesti = tk.Label(tomaatinheitinIkkuna, image=imageErnesti)
+    imageLabelErnesti.image = imageErnesti 
+    imageLabelErnesti.grid(row=random.randint(0, 2), column=2, sticky="e")
+
+def show_ernesti():
+    print("Hei")
+    global onkoPainettu
+    if onkoPainettu == False:
+        image_ernesti()
+        print(onkoPainettu)
+        onkoPainettu = True
+        print(onkoPainettu)
+    else: 
+        print("Ernesti tuli näkyviin")
+    
+
+# Button
+button = tk.Button(
+                tomaatinheitinIkkuna, 
+                text="Show Ernesti",
+                command=show_ernesti,
+                activebackground="black",
+                activeforeground="White",
+                anchor="s",
+                bd=3,
+                bg="lightgray",
+                cursor="hand2",
+                   
+disabledforeground=
+                "gray",
+                fg="black",
+                font=("Arial", 12),
+                height=2,
+                    
+highlightbackground=
+                "black",
+                highlightcolor="green",
+                highlightthickness=2,
+                justify="center",
+                overrelief="raised",
+                padx=5,
+                pady=1,
+                width=15,
+                wraplength=100
+            )
+
+button.grid(row=2, column=1, sticky="s")
 
 tomaatinheitinIkkuna.mainloop()
